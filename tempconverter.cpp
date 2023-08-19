28b60be14e0c1b1dbd1899e23633319d62b040c8
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-19 21:44:00 +0530

    Add files via upload

---
+++ tempconverter.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+	float c1,f1,c2,f2;
+	printf("Enter temperature in Fahrenheit: ");
+	scanf("%f",&f1);
+	c1=(f1-32)*5/9;
+	printf("Temperature in centigrade: %f\n",c1);
+	printf("Enter temperature in Centigrade: ");
+	scanf("%f",&c2);
+	f2=32+(9/5)*c2;
+	printf("Temperature in Fahrenheit: %f",f2);
+	
+	return 0;
+}

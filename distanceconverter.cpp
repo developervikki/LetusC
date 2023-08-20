6b4190accb5cb183ca91116165fd3e0a5167bf31
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-20 14:49:12 +0530

    C program to convert km to Meter,Feets,Inches, cm
    here, a c program to convert km to meter,feets,inches and centimeter. 
    1km=1000 meter
    1km=100000centimeter
    1km=3280.83feets
    1km=39370.078inches

---
+++ distanceconverter.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  float dkm, m,f,i,c;
+  printf("Enter distance between two cities in KM: ");
+  scanf("%f",&dkm);
+  m=dkm*1000;
+  f=dkm*3280.83;
+  i=dkm*39370.078;
+  c=dkm*100000;
+  printf(" distance between two cities in Meters %f \n distance between two cities in Feets %f \n distance between two cities in inches %f \n distance between two cities in centimeters %f ",m,f,i,c);
+  
+  return 0;
+}

2a02fa2b66b4270729d99f768e9724c390beda10
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-20 15:45:13 +0530

    c program for calculating gross salary of employee

---
+++ Grass-salary.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+  int bs; 
+  float da,ra,gs;
+  printf("Enter Ramesh's Basic Salary : ");
+  scanf("%d",&bs);
+  da=bs*40/100;
+  ra=bs*20/100;
+  gs=bs-da-ra;
+  printf("Gross Salary of Ramesh is %f",gs);
+  return 0;
+}

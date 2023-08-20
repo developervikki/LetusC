f3982e4d89296c77b22c48247dd2dbcc103a6ce5
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-20 14:34:39 +0530

    Add files via upload

---
+++ aggregateandpercentagemarks.cpp
@@ -0,0 +1,12 @@
+//write a program to find aggregate and percentage marks
+#include<stdio.h>
+main(){
+	int s1,s2,s3,s4,s5;
+	printf("Enter Five Subjects Marks: \n");
+	scanf("%d%d%d%d%d",&s1,&s2,&s3,&s4,&s5);
+	int agg=s1+s2+s3+s4+s5;
+	float p=agg*100/500;
+	printf("Aggregate marks %d \n" , agg);
+	printf("Percentage marks %f", p);
+	return 0;
+}

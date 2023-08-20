1d4d0b01b7f8cf7e6a69008323938dd056efea37
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-20 14:38:46 +0530

    Area And Perimeter Of a rectangle

---
+++ areaandperimeter.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main(){
+	int l,b,ra,rp;
+	float cr,ca,cc;
+	printf("Enter length and breadth of rectangle:");
+	scanf("%d%d",&l,&b);
+	ra=l*b;
+	rp=2*(l+b);
+	printf("area of rectangle %d \n perimeter of rectangle %d\n", ra,rp);
+	printf("Enter radius of circle:");
+	scanf("%f",&cr);
+	ca=3.14*cr*cr;
+	cc=2*3.14*cr;
+		printf("area of circle %f \n circumference of circle %f\n", ca,cc);
+		return 0;
+
+	
+}

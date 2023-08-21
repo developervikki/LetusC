1809b04bf4e02dffe50b3e433907abea87f594e2
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-21 22:45:00 +0530

    Calculator
    a c program to make calculator for two  number addition, substraction, Multiplication and division.

---
+++ calculator.c
@@ -0,0 +1,35 @@
+//Write a Program to make a simple calculator based on user choice
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n1,n2,res=0,ch;
+	printf("Enter Two Number:");
+	scanf("%d%d",&n1,&n2);
+	printf("Chose For Calculation\n");
+	printf("chose 1 for addition\n ");
+	printf("chose 2 for substraction\n ");
+	printf("chose 3 for Multiplextion\n ");
+	printf("chose 4 for Division\n ");
+	scanf("%d",&ch);
+	switch(ch)
+	{
+		case 1:
+		    res=n1+n2;
+		break;
+		case 2:
+		    res=n1-n2;
+		break;
+		case 3:
+		    res=n1*n2;
+		break;
+		case 4:
+		    res=n1/n2;
+		break;
+		default:
+			printf("Invalid Choice\n");
+		
+	}
+	printf("Result=%d\n",res);
+	
+}

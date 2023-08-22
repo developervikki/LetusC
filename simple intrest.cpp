9f1aa671d012604ca609478af665a95ef760fde4
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-22 19:08:12 +0530

    a c Program to find Simple interest.
    a c Program to find Simple interest.

---
+++ simple intrest.cpp
@@ -0,0 +1,18 @@
+//Write a Program m to calculate simple interest
+//si=(p*n*r)/100; 
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	
+	float p,n,r,si;
+	printf("Principal Value: ");
+	scanf("%f", &p);
+	printf("Time: ");
+	scanf("%f", &n);
+	printf("Rate Value: ");
+	scanf("%f", &r);
+	si=(p*n*r)/100;
+	printf("Simple Intrest=%f ~", si);
+	return 0;
+}

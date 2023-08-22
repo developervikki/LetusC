ec4f1d69577d1b4b2413bdbcec2db4c8e89d785a
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-22 19:19:19 +0530

    electricity bill calculator
    electricity bill calculator

---
+++ electricity bill calculator.cpp
@@ -0,0 +1,24 @@
+//elctricity bill calculator
+#include<stdio.h>
+main()
+{
+    int unit;
+    float bill;
+    printf("Enter number of unit: ");
+    scanf("%d",&unit);
+    if(unit<=150)
+    {
+        bill=unit*2.40;
+    }
+    else if (unit>150 && unit<=300)
+    {
+        bill=(150*2.40)+(unit-150)*3.00;
+    }
+    else
+    {
+        bill=(150*2.40)+(150*3.00)+(unit-300)*3.20;
+    }
+    printf("bill=%.2f",bill);
+    
+    return 0;
+}

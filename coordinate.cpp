58c5f83e701a17f5055b47ebbf0180dd0f364e68
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-23 18:38:24 +0530

    determine iquadrant
    a Program to accept a coordinate point in a XY coordinate system and determine its quadrant

---
+++ coordinate.cpp
@@ -0,0 +1,24 @@
+//  Write a Program to accept a coordinate point in a XY coordinate system and determine its quadrant
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x,y;
+    printf("Enter the value of x and y Coordinate: ");
+    scanf("%d%d",&x,&y);
+    
+    if(x>0 && y>0)
+        printf("The coordinate point (%d,%d) lies in the First quadrant",x,y);
+    else if(x<0 && y>0)
+          printf("The coordinate point (%d,%d) lies in the second quadrant",x,y);
+    else if(x<0 && y<0)
+          printf("The coordinate point (%d,%d) lies in the third quadrant",x,y);
+    else if(x>0 && y<0)
+          printf("The coordinate point (%d,%d) lies in the Fourth quadrant",x,y);
+    else if(x==0 && y==0)
+          printf("The coordinate point (%d,%d) lies in the origin",x,y);
+             
+      return 0;
+}
+
+

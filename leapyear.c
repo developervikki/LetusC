789aea3660dd030151aeea7d351cf1e413283a38
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-25 18:44:23 +0530

    a program to find leap year
    a program to find leap year

---
+++ leapyear.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int y;
+	printf("Enter Year: ");
+	scanf("%d",&y);
+	if(y%4==0){
+		printf("This is a Leap year");
+	}
+	else{
+		printf("This is a not leap year");
+	}
+	return 0;
+}

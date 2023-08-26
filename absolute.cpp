487c3bf1fe7da79c8d45de7dadcc3fcc3137799e
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-26 21:53:16 +0530

    a program to find absolute value.
    a program to find absolute value.

---
+++ absolute.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main(){
+	int num,a;
+	printf("Enter A number: ");
+	scanf("%d",&num);
+	
+	if(num<0){
+		a=-(num);
+		printf("Absolute value is %d",a);
+	}
+	else
+	{
+		
+		printf("Absolute value is  %d",num);
+	}
+}

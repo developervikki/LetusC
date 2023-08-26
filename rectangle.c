52cd53b54e414450136c3f282df69c3ccfc0bba5
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-26 21:45:02 +0530

    a program to find area of the rectangle is grater than its perimeter.
    a program to find area of the rectangle is grater than its perimeter.

---
+++ rectangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main(){
+	int l,b,a,p;
+	printf("Enter lenght of Rectangle: ");
+	scanf("%d",&l);
+	printf("Enter breadth of Rectangle: ");
+	scanf("%d",&b);
+	a=l*b;
+	p=2*l+2*b;
+	if(a>p){
+		printf("Area of ractangle is grater than Perimeter");
+	}
+	else{
+		printf("perimter of rectangle is greater than area");
+	}
+	return 0;
+}

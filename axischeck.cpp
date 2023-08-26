3641cadebf9973b492223265b0c8ca52bb6c633b
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-26 21:52:10 +0530

    a program to find out if it lies on the X-axis,  Y-axis or on origion.
    a program to find out if it lies on the X-axis,  Y-axis or on origion.

---
+++ axischeck.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main(){
+	int x,y;
+	printf("Enter x and y: \n");
+	scanf("%d%d",&x,&y);
+	if(x>y){
+		printf("it is lies on x-axis");
+	}
+	else if(y>x){
+		printf("it is lies on y-axis");
+	}
+	else{
+		printf("it is lies on origion");
+	}
+}

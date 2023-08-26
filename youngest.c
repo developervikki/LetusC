20f6f3d6d426e77db27e64497519e49a0555b310
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-26 21:40:10 +0530

    a program to determine youngest of the three
    a program to determine youngest of the three.

---
+++ youngest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main(){
+	int a,b,c;
+	printf("Enter Ram Age: ");
+	scanf("%d",&a);
+	printf("Enter Shyam Age: ");
+	scanf("%d",&b);
+	printf("Enter Ajay Age: ");
+	scanf("%d",&c);
+	if(a<b && a<c){
+		printf("Ram youngest of the three");
+	}
+	else if(b<a && b<c){
+		printf("Shyam youngest of the three");
+	}
+	else{
+		printf("AJay youngest of the three");
+	
+	}
+	return 0;
+	
+}

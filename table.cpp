a3f73e66714e4fc48facff2a88ca15d0f4062460
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-27 20:59:41 +0530

    a c program to write table
    a c program to write table

---
+++ table.cpp
@@ -0,0 +1,16 @@
+
+//write a program to write table to given number
+#include<stdio.h>
+main()
+{
+ int number,table;
+ printf("Table \n");
+    printf("Enter A number: ");
+     scanf("%d",&number);
+	  for (int i = 1; i <= 10; i++)
+	  
+	{
+      table=i*number;
+         printf("%d\n",table);
+}
+}

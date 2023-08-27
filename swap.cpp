5f2c7c482c5af7eb5b9320c4c0c47a21a190a8dd
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-27 21:01:16 +0530

    a c program to swap twio numbers.
    a c program to swap twio numbers.

---
+++ swap.cpp
@@ -0,0 +1,19 @@
+//WAP to swap two number using call by refrence
+#include<stdio.h>
+void swap(int *,int *);
+main()
+{
+	int a,b;
+	printf("Enter two number:");
+	scanf("%d%d",&a,&b);
+	swap(&a,&b);//calling of function
+	printf("a=%d\nb=%d\n",a,b);
+	
+}
+void swap(int*x,int*y)
+{
+	int t;
+	t=*x;
+	*x=*y;
+	*y=t;
+}

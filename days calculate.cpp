00fc8b81bff51360aa4705323d87241830f5abd2
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-28 23:06:10 +0530

    a c program to find years,weeks, days
    a c program to find years,weeks, days

---
+++ days calculate.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int ndays, years, weeks, days;
+	printf("Enter Number Of  days: ");
+	scanf("%d", &ndays);
+	years=ndays/365;
+	weeks=(ndays%365)/7;
+	days=(ndays%365)%7;
+	printf("Years: %d week: %d days: ",years,weeks,days);
+	
+}

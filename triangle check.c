8e96af87d48a2ca52729f23e231656016112e6ce
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-08-28 23:02:27 +0530

    a c program to find isoceles,equilateral,scalene or right angle triangle.
    a c program to find isoceles,equilateral,scalene or right angle triangle.

---
+++ triangle check.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main(){
+	int a,b,c,d;
+	printf("Enter three sides of triangle: ");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a==b==c){
+		printf("Equilateral Triangle");
+	}
+	else if (a=b || a==c){
+		printf("Isoceles Triangle");
+	}
+	else if (c=a^2+b^2)
+	{
+		printf("Right Angle Triangle");
+	}
+	else{
+		printf("scalene Triangle");
+	}
+	return 0;
+}

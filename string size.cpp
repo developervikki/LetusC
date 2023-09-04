3cc8f8ad44d7e137782d8be31806a9ca98b1990e
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-09-04 19:35:39 +0530

    A c program to find string size.
    A c program to find string size.

---
+++ string size.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define MAX_SIZE 100
+int main()
+{
+	char text[MAX_SIZE];
+	int i;
+	int count=0;
+	printf("Enter Any String: ");
+	gets(text);
+	for(i=0;text[i]!='\0'; i++)
+	{
+		count++;
+	}
+	printf("Length of '%s'=%d",text, count);
+	return 0;
+	
+	}

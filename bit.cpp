0571b02ed1feaa673d8c8fa462cd3078234483dd
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-09-04 19:25:43 +0530

    a c program to find data types size in c language
    a c program to find data types size in c language

---
+++ bit.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+ main()
+{
+	
+printf("char : %d Bytes\n", sizeof(char));
+printf("short : %d Bytes\n", sizeof(short));
+printf("int : %d Bytes\n", sizeof(int));
+printf("long : %d Bytes\n\n", sizeof(long));
+printf("signed char : %d Bytes\n", sizeof(signed char));
+printf("signed short : %d Bytes\n", sizeof(signed short));
+printf("signed int : %d Bytes\n", sizeof(signed int));
+printf("signed long : %d Bytes\n\n", sizeof(signed long));
+printf("unsigned char : %d Bytes\n", sizeof(unsigned char));
+printf("unsigned short: %d Bytes\n", sizeof(unsigned short));
+printf("unsigned int : %d Bytes\n", sizeof(unsigned int));
+printf("unsigned long : %d Bytes\n\n", sizeof(unsigned long));
+printf("float : %d Bytes\n", sizeof(float));
+printf("double : %d Bytes\n", sizeof(double));
+}

6ebf200d9367f72d38a2bc3ce3025cc3d926c1f2
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-09-06 19:25:51 +0530

    a simple C program that uses a for loop to print the numbers from 1 to 10.
    a simple C program that uses a for loop to print the numbers from 1 to 10.

--- 1to10.c
+++ 1to10.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1; i <= 10; i++) {
-        printf("%d\n", i);
+	int i=1;
+    for (i; i <= 10; i++) {
+        printf("%d ", i);
     }
     return 0;
 }

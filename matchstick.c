31301d27f6dd7088acd635e8b1b4353b7869d2cd
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-09-07 17:51:33 +0530

    A PROGRAM FOR A MATCHSTICK GAME.
    A PROGRAM FOR A MATCHSTICK GAME BEING PLAYED BETWEEN THE COMPUTER AND A USER.

---
+++ matchstick.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int ms = 21, uc, cc;
+    while(ms>=1)
+    {
+        printf("Total Match Sticks: %d\n", ms);
+        printf("Pick up the match sticks between (1 to 4): ");
+        scanf("%d", &uc);
+
+        if(uc>4)
+        {
+            printf("Invalid Entry");
+            break;
+        }
+        cc = 5 - uc;
+        printf("Computer picks up the %d match sticks.\n", cc);
+        ms = ms-uc-cc;
+        if(ms==1)
+        {
+            printf("\n computer win.");
+            break;
+        }
+    }
+    return(0);
+}

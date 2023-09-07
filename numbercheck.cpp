e59757f3bd8badc46ac857c17744e7752a42b075
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-09-07 17:49:03 +0530

    A C PROGRAM TO CHECK GIVEN NUMBER POSITIVE, NEGATIVE OR ZERO.
    A C PROGRAM TO CHECK GIVEN NUMBER POSITIVE, NEGATIVE OR ZERO.

---
+++ numbercheck.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main(){
+	float num;
+	printf("Enter the Number: ");
+	scanf("%f",&num);
+	if(num>0){
+		printf("Positive number");
+	}
+	else if(num<0){
+		printf("Negative number");
+
+	}
+	else
+	{
+		printf("You Entered Zero");
+	}
+}

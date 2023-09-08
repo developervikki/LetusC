142a4a30b60741dbc6694ed308029ad5140e9dc2
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-09-08 17:40:50 +0530

    a c program top calculate overtime pay of 10 empolyees.
    a c program top calculate overtime pay of 10 empolyees.

---
+++ overtime.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main(){
+	float otpay;
+	int hour, i=1;
+	while(i<=10){
+		printf("Enter number of hours worked: ");
+		scanf("%d",&hour);
+		if(hour>=40){
+			otpay=(hour-40)*12;
+		}
+		else
+		otpay=0;
+		printf("Hourse=%d overtimepay=Rs=%f\n",hour,otpay);
+		i++;
+	}
+}

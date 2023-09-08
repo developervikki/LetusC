c2ee11c8c88edb4677bcdfe558611ce6773100d1
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-09-08 17:42:09 +0530

    a c program to fiond value of one number raised to the power another number.
    a c program to fiond value of one number raised to the power another number.

---
+++ power.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main(){
+	float x, power;
+	int y,i;
+	printf("\nEnter Two Number: ");
+	scanf("%f%d",&x,&y);
+	power=i=1;
+	while(i<=y){
+		power=power*x;
+		i++;
+	}
+	printf("%f to the power %d is %f\n",x,y,power);
+}

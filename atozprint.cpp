f85500b1f3976388093065a9067fa7def7d8486a
Vikash Yadav <91186160+Vky9670480893@users.noreply.github.com>
2023-09-09 16:59:11 +0530

    Program to print alphabets from a-z
    Program to print alphabets from a-z

---
+++ atozprint.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Alphabets from a - z are: \n");
+    for(ch='a'; ch<='z'; ch++)
+    {
+        printf("%c \t\n", ch);
+    }
+
+    return 0;
+}
+
+

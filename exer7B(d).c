5c7616530cb951fb830faf61a541576ed62953e7
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-09-24 20:02:49 +0530

    Chapter 7 Case control Instruction solution..
    Chapter 7 Case control Instruction solution..[B](d)

---
+++ exer7B(d).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+	int a =3,b=4,c;
+	c=a-b;
+	switch(c){
+		case 1||2:
+			printf("God give me a chance to change things \n");
+			break;
+		case a||b:
+			printf("God give me a chance to run my show \n");
+			break;
+	}
+	return 0;
+}

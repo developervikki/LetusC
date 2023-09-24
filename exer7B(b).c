166ab9227f8a9ffbe710f500f1112a5aebcb4a69
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-09-24 20:01:21 +0530

    Chapter 7 Case control Instruction solution..
    Chapter 7 Case control Instruction solution..[B](c)

---
+++ exer7B(b).c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+main(){
+	int temp;
+	printf("Enter the value of temp: ");
+	scanf("%d",&temp);
+	/*switch(temp)
+	{
+		case (temp<=20):
+			printf("Oooooh! Damn Cool ! \n");
+		case(temp>20 && temp<30):
+			printf("Rain rain here again \n");
+		case(temp>30 && temp<=40):
+			printf("Wish I am on Everest \n");
+		deafult:
+			printf("Good old nagpur weather \n");
+	}*/
+	if(temp<=20)
+	{
+		goto damn;
+	}
+	else if(temp>20 && temp<30){
+		goto Rain;
+		
+	}
+	else if(temp>30 && temp<=40)
+	{
+		goto Everest;
+	}
+	else{
+		printf("Good old nagpur weather \n");
+	}
+	damn:
+		printf("Oooooh! Damn Cool ! \n");
+	Rain:
+		printf("Rain rain here again \n");
+	Everest:
+		printf("Wish I am on Everest \n");
+
+
+	return 0;
+}

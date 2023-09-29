becadc5defc3528d95aa05afe007cf136dea74c2
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-09-29 18:29:55 +0530

    Write a program to draw a hollow rectangle..
    Write a program to draw a hollow rectangle..

---
+++ HOLLOWRECT.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int i, j, rows, cols;
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= cols; j++) {
+            if (i == 1 || i == rows || j == 1 || j == cols) {
+                printf("* ");
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+

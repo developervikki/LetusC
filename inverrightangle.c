02ab7c55fd2ae24b5df1e9ea4c17654d419f4030
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-09-29 18:28:10 +0530

    WAP to draw a inverse right angle triangle
    WAP to draw a inverse right angle triangle

---
+++ inverrightangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int rows, i, j;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    for (i = rows; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+

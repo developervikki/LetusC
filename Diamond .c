90d922169342e87e89f5d9db225c3a8d637c7d9f
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-09-29 18:31:27 +0530

    write a program to draw a pyramid in  c language
    write a program to draw a pyramid in  c language

---
+++ Diamond .c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int i, j, space, rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++) {
+        for (space = 1; space <= rows - i; space++) {
+            printf("  ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for (i = rows - 1; i >= 1; i--) {
+        for (space = 1; space <= rows - i; space++) {
+            printf("  ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+

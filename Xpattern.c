8c58a9b8208b957230711cd962a6812da8cf999c
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-01 19:25:17 +0530

    Write a program to draw 'X' Latter Pattern
    Write a program to draw 'X' Latter Pattern

---
+++ Xpattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main(){
+	int i,j,n=5,s;
+	for(i=1; i<=n; i++){
+		for(j=1; j<=n; j++){
+			if(i==j || i+j==n+1){
+				printf("X");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}

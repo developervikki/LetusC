6277ec272f2eabc638f2c47b0bf1310513b929b2
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-02 23:27:58 +0530

    A program to draw a V latter pattern in c language.
    A program to draw a V latter pattern in c language.

---
+++ V.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main(){
+	int i,j,n=7,s;
+	for(i=1; i<=4; i++){
+		for(j=1; j<=n; j++){
+			if(i==j || i+j==n+1){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}

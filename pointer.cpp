b289bc7bb28fba56a3d9ac785ae5798bc81a2f35
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-02 23:08:07 +0530

    WAP to demonstrate Concept of Pointer
    WAP to demonstrate Concept of Pointer

---
+++ pointer.cpp
@@ -0,0 +1,11 @@
+//WAP to demonstrate Concept of Pointer
+#include<stdio.h>
+main()
+{
+	int x=400;
+	int*p;
+	p=&x;
+	printf("Value Of x=%d\n",x);
+	printf("Value Of P Address=%d\n",p);
+	printf("Value of P=%d\n",*p);
+}

c22f0d7f606e5bd5b963b27da6e26f0200708e5f
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-04 19:36:37 +0530

    write a c program to draw a 'A' letter using star pattern in c language.
    write a c program to draw a 'A' letter using star pattern in c language.

---
+++ Apattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,k,height = 7; 
+
+    for(i = 1; i <= height; i++) {
+        for (j = 1; j <= height - i; j++) {
+            printf(" ");
+        }
+
+        for (k = 1; k <= (2 * i) - 1; k++) {
+            if (k == 1 || k == (2 * i) - 1 || i == height / 2 + 1) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}

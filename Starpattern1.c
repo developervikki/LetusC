a0ca88af6e29cd13f09d28dc7f0cdcc5c4b59994
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-12 21:45:19 +0530

    write a program to daw star pattern in c language
    write a program to daw star pattern in c language

---
+++ Starpattern1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+	int n,row,col,space;
+	printf("Enter the value of N:");
+	scanf("%d",&n);
+	for(row=1; row<=n; row++){
+		for(space=1; space<=n-row; space++){
+			printf(" ");
+		}
+		for(col=1;col<=row; col++){
+			printf("* \n");
+			
+		}
+	}
+}

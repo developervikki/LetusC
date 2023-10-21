10be5b77e12af13fbf92e117dd504f78bc521694
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-21 22:11:52 +0530

    Update rightangletri.c

--- rightangletri.c
+++ rightangletri.c
@@ -4,7 +4,7 @@ int main() {
     printf("Enter number of rows: ");
     scanf("%d", &rows);
     for (i = 1; i <= rows; i++) {
-        for (j = 1; j <= i; j++) {
+        for (j = 1; j <= i; j++) { 
             printf("* ");
         }
         printf("\n");

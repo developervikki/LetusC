f32dc49e121f60731b9705b903cd4b5054da3be8
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-26 18:38:22 +0530

    Update atozprint.cpp

--- atozprint.cpp
+++ atozprint.cpp
@@ -7,7 +7,7 @@ int main()
     printf("Alphabets from a - z are: \n");
     for(ch='a'; ch<='z'; ch++)
     {
-        printf("%c \t\n", ch);
+        printf("%c \t\n", ch); 
     }
 
     return 0;

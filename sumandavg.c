4c5e1c172258832d798f8d29e17d73dd9881bc33
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-28 11:27:36 +0530

    write a program to read marks of 5 students calculate sum and avg  using array.
    write a program to read marks of 5 students calculate sum and avg  using array.

---
+++ sumandavg.c
@@ -0,0 +1,17 @@
+//write a program to read marks of 5 students calculate sum and avg  using array.
+#include<stdio.h>
+void main(){
+	int marks[5], i;
+	float sum=0,avg;
+	printf("Enter the marks:");
+	for(i=0;i<5;i++){
+		scanf("%d",&marks[i]);
+	 }
+	for(i=0; i<5; i++){
+		sum=sum+marks[i];
+		
+	}
+	avg=sum/5;
+	printf("sum= %f \n",sum);
+	printf("average= %f",avg);
+}

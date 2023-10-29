b4b74856d7f1a09cc7e514348fda40b3095f8ea8
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-29 12:07:47 +0530

    // program to print multiplication of two matrices
    // program to print multiplication of two matrices

---
+++ matrxmulti.c
@@ -0,0 +1,31 @@
+// program to print multiplication of two matrices
+#include<stdio.h>
+#include<stdio.h>
+main(){
+	int a[3][3],b[3][3],c[3][3],i,j,sum, k;
+	printf("Enter the first matrix Elements: ");
+	for(i=0; i<3; i++){
+		for(j=0;j<3; j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Enter the Second matrix Elements: ");
+	for(i=0; i<3; i++){
+		for(j=0;j<3; j++){
+			scanf("%d",&b[i][j]);
+		}
+	}
+	for(i=0; i<3; i++){
+		for(j=0;j<3; j++){
+			sum=0;
+			for(k=0;k<3; k++){
+			sum=sum+a[i][k]*b[k][j];
+			c[i][j]=sum;
+			printf("%d \t", c[i][j]);
+			
+			}
+		}
+		printf("\n");
+	}
+	
+}

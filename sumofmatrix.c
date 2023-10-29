84f772a5fd62322f74f61f25e14114d1c2ae6eb1
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-29 12:06:35 +0530

    //write a program to print addition of two matrices
    //write a program to print addition of two matrices

---
+++ sumofmatrix.c
@@ -0,0 +1,25 @@
+//write a program to print addition of two matrices
+#include<stdio.h>
+main(){
+	int a[2][3],b[2][3],c[2][3],i,j;
+	printf("Enter the first matrix Elements: ");
+	for(i=0; i<2; i++){
+		for(j=0;j<3; j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Enter the Second matrix Elements: ");
+	for(i=0; i<2; i++){
+		for(j=0;j<3; j++){
+			scanf("%d",&b[i][j]);
+		}
+	}
+	for(i=0; i<2; i++){
+		for(j=0;j<3; j++){
+			c[i][j]=a[i][j]+b[i][j];
+			printf("%d \t", c[i][j]);
+		}
+		printf("\n");
+	}
+	
+}

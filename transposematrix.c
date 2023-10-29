f9498c729765238e2723a2f118d2965f660819c6
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-29 12:05:18 +0530

    //WAP to print transpose of a matrix
    //WAP to print transpose of a matrix

---
+++ transposematrix.c
@@ -0,0 +1,17 @@
+//WAP to print transpose of a matrix
+#include<stdio.h>
+void main( ){
+	int a[2][3],i,j,sum=0;
+	printf("Enter the elements: ");
+	for(i=0; i<2; i++){
+		for(j=0; j<3; j++){
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++){
+		for(j=0; j<2; j++){
+			printf("\t %d \t",a[j][i]);
+		}
+		printf("\n");
+	}
+}

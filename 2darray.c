e1db7e5989be98f08c211afc2e95b045f6776e42
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-29 12:04:27 +0530

    // PROGRAM TO PRINT 2D ARRAY & CALCULATE SUM
    // PROGRAM TO PRINT 2D ARRAY & CALCULATE SUM

---
+++ 2darray.c
@@ -0,0 +1,19 @@
+// pROGRAM TO PRINT 2D ARRAY & CALCULATE SUM
+#include<stdio.h>
+void main(){
+	int a[2][3],i,j,sum=0;
+	printf("Enter the elements: ");
+	for(i=0; i<2; i++){
+		for(j=0; j<3; j++){
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for(i=0; i<2; i++){
+		for(j=0; j<3; j++){
+			printf("%d \t",a[i][j]);
+			sum=sum+a[i][j];
+		}
+		printf("\n");
+	}
+	printf("sum %d", sum);
+}

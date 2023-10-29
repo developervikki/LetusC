3033468a6d5c4833b5f6c5ead904358589ad1359
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-29 12:05:56 +0530

    //Program to point sum of individual rows and columns
    //Program to point sum of individual rows and columns

---
+++ sumrowandcoloumn.c
@@ -0,0 +1,19 @@
+//Program to point sum of individual rows and columns
+#include<stdio.h>
+main(){
+	int a[3][3],i,j,sr,sc;
+	printf("Enter the Elements: ");
+	for(i=0; i<3; i++){
+		for(j=0;j<3;j++){
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++){
+		sr=sc=0;
+		for(j=0;j<3;j++){
+			sr=sr+a[i][j];
+			sc=sc+a[j][i];
+		}
+		printf("SR= %d, SC=%d\n",sr,sc);
+	}
+}

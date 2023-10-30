c0adf019962c0d11148836d1039333a9efae04e5
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-30 19:43:34 +0530

    Write a program in C to copy the elements of one array into another array.
    Write a program in C to copy the elements of one array into another array.

---
+++ arracpoy.c
@@ -0,0 +1,13 @@
+//Write a program in C to copy the elements of one array into another array.
+#include<stdio.h>
+main(){
+	int a[3],b[3],i;
+	printf("Enter the Elements: ");
+	for(i=0; i<3; i++){
+		scanf("%d",&a[i]);
+		b[i]=a[i];
+	}
+	for(i=0; i<3; i++){
+	printf("%d",b[i]);
+ }
+}

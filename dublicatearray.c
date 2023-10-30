c5cc987e1fa20f95708751134155f0b0788d4c92
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-10-30 19:44:16 +0530

    Write a program to find dublicate in an Array Elements..
    Write a program to find dublicate in an Array Elements..

---
+++ dublicatearray.c
@@ -0,0 +1,26 @@
+//Write a program to find dublicate in an Array Elements..
+#include <stdio.h>
+int main()
+{
+   int arr[100];
+   int n,ctr=0;
+   int i, j;
+     printf("Input the number of elements to be stored in the array :");
+      scanf("%d",&n);
+      printf("Input %d elements in the array :\n",n);
+      for(i=0;i<n;i++)
+       {
+              printf("element - %d : ",i);
+              scanf("%d",&arr[i]);
+            }
+          for (i = 0; i < n; i++) {
+      for (j = i + 1; j < n; j++) {
+       if (arr[i] == arr[j]) {
+         ctr++;
+         break;
+   }
+   }
+ }
+ printf("Total number of duplicate elements found in the array: %d\n", ctr);
+ return 0;
+}

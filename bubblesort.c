05636e1f5f1ed7271e2a4da1dbf49faa85d5f9f9
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-11-04 19:52:53 +0530

    Implementation the bubble sort in c language..
    Implementation the bubble sort in c language..

---
+++ bubblesort.c
@@ -0,0 +1,35 @@
+//Implementation the bubble sort in c language..
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+

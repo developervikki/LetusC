220cc60f82664cfccd46797f38048ef4e13cce2d
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-11-07 20:03:27 +0530

    Update inverrightangle.c

--- inverrightangle.c
+++ inverrightangle.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d", &rows);
     for (i = rows; i >= 1; i--) {
         for (j = 1; j <= i; j++) {
-            printf("* ");
+            printf("* "); 
         }
         printf("\n");
     }

5d9459c6d4e96ac0e986f42d4b6682f3d4b6aa07
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-11-09 19:16:24 +0530

    Update Diamond .c

--- Diamond .c
+++ Diamond .c
@@ -8,7 +8,7 @@ int main() {
             printf("  ");
         } 
         for (j = 1; j <= 2 * i - 1; j++) {
-            printf("* ");
+            printf("* "); 
         }
         printf("\n");
     }

c6f1d21b099de654e9b49eb2d9fa42929f6f12ee
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-11-10 20:41:12 +0530

    Update leapyear.c

--- leapyear.c
+++ leapyear.c
@@ -5,7 +5,7 @@ main()
 	printf("Enter Year: ");
 	scanf("%d",&y);
 	if(y%4==0){
-		printf("This is a Leap year");
+		printf("This is a Leap year"); 
 	}
 	else{
 		printf("This is a not leap year");

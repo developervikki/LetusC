dc3d3cc86f6284a512635a3f1e96921c8b6d4a81
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-11-13 20:17:07 +0530

    check equality array in c language.
    check equality array in c language.

---
+++ equalityarray.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{{
+	int a,b;
+	printf("Enter Five Digit Numbber: ");
+	scanf("%d",&a);
+	printf("Enter Reverse Number Of Above Numbber:");
+	scanf("%d",&b);
+	if(a==b)
+	{
+		printf("Given Original And Reverse Number Equal");
+	}
+	else
+	{
+		printf("Given Original And Reverse Numbber Are not equal");
+	}
+}}

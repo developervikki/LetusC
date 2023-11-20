d6a48a5fc9541cde5e7289891bc5ec06621ff7b2
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-11-20 08:12:51 +0530

    Update HOLLOWRECT.c

--- HOLLOWRECT.c
+++ HOLLOWRECT.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d %d", &rows, &cols);
     for (i = 1; i <= rows; i++) { 
         for (j = 1; j <= cols; j++) {
-            if (i == 1 || i == rows || j == 1 || j == cols) {
+            if (i == 1 || i == rows || j == 1 || j == cols) { 
                 printf("* ");
             } else {
                 printf("  ");

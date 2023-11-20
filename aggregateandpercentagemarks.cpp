5d4851c8c71944c300644e4c0df4e796ac4ae6bf
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-11-20 08:13:00 +0530

    Update aggregateandpercentagemarks.cpp

--- aggregateandpercentagemarks.cpp
+++ aggregateandpercentagemarks.cpp
@@ -4,7 +4,7 @@ main(){
 	int s1,s2,s3,s4,s5;
 	printf("Enter Five Subjects Marks: \n");
 	scanf("%d%d%d%d%d",&s1,&s2,&s3,&s4,&s5); 
-	int agg=s1+s2+s3+s4+s5;
+	int agg=s1+s2+s3+s4+s5; 
 	float p=agg*100/500;
 	printf("Aggregate marks %d \n" , agg);
 	printf("Percentage marks %f", p);

fd76f431f165c5dd55272577cb0411e1f69a54a0
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-12-03 21:25:36 +0530

    Update Starpattern1.c

--- Starpattern1.c
+++ Starpattern1.c
@@ -5,7 +5,7 @@ main(){
 	printf("Enter the value of N:");
 	scanf("%d",&n);
 	for(row=1; row<=n; row++){  
-		for(space=1; space<=n-row; space++){
+		for(space=1; space<=n-row; space++){ 
 			printf(" ");
 		}
 		for(col=1;col<=row; col++){

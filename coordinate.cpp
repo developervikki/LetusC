78a67d978c769e62805c4afda8e74a7a02a6475c
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-12-05 23:01:52 +0530

    Update coordinate.cpp

--- coordinate.cpp
+++ coordinate.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int x,y;
     printf("Enter the value of x and y Coordinate: ");
-    scanf("%d%d",&x,&y);
+    scanf("%d%d",&x,&y); 
     
     if(x>0 && y>0)
         printf("The coordinate point (%d,%d) lies in the First quadrant",x,y); 

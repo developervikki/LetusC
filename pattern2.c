ac15eec38fef6aa260c985fc4bcb25e5de5335bc
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-12-15 20:43:19 +0530

    Update pattern2.c

--- pattern2.c
+++ pattern2.c
@@ -5,7 +5,7 @@ int main()
   
     printf("How Many lines: ");
 	scanf("%d",&n);
-    for(i=0; i<n;i++) {
+    for(i=0; i<n;i++) { 
         for(j = 0; j<= i; j++) {
             printf("* ");
         }

e958b8d5a6032a015fb51cf9a7d03aae14d48570
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-12-21 19:39:20 +0530

    Update PYRAMID.c

--- PYRAMID.c
+++ PYRAMID.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int rows, i, j, space;
-    printf("Enter number of rows: ");
+    printf("Enter number of rows: "); 
     scanf("%d", &rows);
     for (i = 1; i <= rows; i++) {
         for (space = 1; space <= rows - i; space++) {

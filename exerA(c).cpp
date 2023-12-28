488f0baf3efe7496fc0e630ea74ff33acc315299
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2023-12-28 16:06:26 +0530

    Update exerA(c).cpp

--- exerA(c).cpp
+++ exerA(c).cpp
@@ -6,7 +6,7 @@ main(){
 			printf("Customers are dicey \n"); 
 		case 1+0:
 			printf("Markets are pricey \n");
-		case 4/2:
+		case 4/2: 
 			printf("Investors are moody \n");
 		case 8%5:
 			printf("At least employees are good \n");

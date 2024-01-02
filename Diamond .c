9d4ab20f0190dde46211895bf68929e065379f17
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-01-02 23:44:32 +0530

    Update Diamond .c

--- Diamond .c
+++ Diamond .c
@@ -3,7 +3,7 @@ int main() {
     int i, j, space, rows;
     printf("Enter the number of rows: ");
     scanf("%d", &rows);
-    for (i = 1; i <= rows; i++) {
+    for (i = 1; i <= rows; i++) { 
         for (space = 1; space <= rows - i; space++) {  
             printf("  "); 
         } 

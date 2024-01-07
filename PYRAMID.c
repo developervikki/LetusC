34b5469607e382f0302309fc04d31829f135d2ab
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-01-07 21:22:22 +0530

    Update PYRAMID.c

--- PYRAMID.c
+++ PYRAMID.c
@@ -6,7 +6,7 @@ int main() {
     for (i = 1; i <= rows; i++) {
         for (space = 1; space <= rows - i; space++) {
             printf(" ");
-        }
+        } 
         for (j = 1; j <= 2 * i - 1; j++) { 
             printf("*");
         }  

fc96fe3c87dcdafba620220577b8d151c0851354
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-01-22 20:28:29 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -12,7 +12,7 @@ int main() {
     printf("Enter %d elements:\n", n);
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
-    }
+    } 
  
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - 1 - i; j++) {

fc51cb68c6f8b3e7b3d65ea79ceefe460ea56692
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-02-15 23:29:13 +0530

    Update sumofmatrix.c

--- sumofmatrix.c
+++ sumofmatrix.c
@@ -4,7 +4,7 @@ main(){
 	int a[2][3],b[2][3],c[2][3],i,j;
 	printf("Enter the first matrix Elements: ");
 	for(i=0;i<2;i++){
-		for(j=0;j<3; j++){
+		for(j=0;j<3; j++){ 
 			scanf("%d",&a[i][j]);
 		}
 	}

205145f02f279d064c589bba8fb9436c0d1ef590
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-02-17 23:37:07 +0530

    Update Diamond .c

--- Diamond .c
+++ Diamond .c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d", &rows);
     for (i = 1; i <= rows; i++) { 
         for (space = 1; space <= rows - i; space++) {  
-            printf("  "); 
+            printf("  ");  
         } 
         for (j = 1; j <= 2 * i - 1; j++) {
             printf("* "); 

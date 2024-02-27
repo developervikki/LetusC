03e329f863b83d0eb4956d08db36190f3f96216f
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-02-27 23:01:15 +0530

    Update arracpoy.c

--- arracpoy.c
+++ arracpoy.c
@@ -1,7 +1,7 @@
 //Write a program in C to copy the elements of one array into another array.
 #include<stdio.h>
 main(){
-	int a[3],b[3],i;
+	int a[3],b[3],i; 
 	printf("Enter the Elements: ");
 	for(i=0; i<3; i++){ 
 		scanf("%d",&a[i]);

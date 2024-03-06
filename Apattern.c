f17c7244f118bf9f5b5bc050d33bd2156ba64d4f
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-03-06 20:52:15 +0530

    Update Apattern.c

--- Apattern.c
+++ Apattern.c
@@ -7,7 +7,7 @@ int main() {
             printf(" "); 
         }  
    
-        for (k = 1; k <= (2 * i) - 1; k++) {
+        for (k = 1; k <= (2 * i) - 1; k++) { 
             if (k == 1 || k == (2 * i) - 1 || i == height / 2 + 1) {  
                 printf("*");
             } else { 

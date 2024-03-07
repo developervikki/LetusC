8a245f7765076513d5e85a832994c4f7bd5532dc
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-03-07 19:34:50 +0530

    Update equalityarray.cpp

--- equalityarray.cpp
+++ equalityarray.cpp
@@ -5,7 +5,7 @@ main()
 	printf("Enter Five Digit Numbber: ");
 	scanf("%d",&a);
 	printf("Enter Reverse Number Of Above Numbber:");
-	scanf("%d",&b);
+	scanf("%d",&b); 
 	if(a==b)
 	{
 		printf("Given Original And Reverse Number Equal");

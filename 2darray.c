1fb4587dee396f406f9c87b86cd2e45cef05c66f
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-03-14 23:44:13 +0530

    Update 2darray.c

--- 2darray.c
+++ 2darray.c
@@ -4,7 +4,7 @@ void main(){
 	int a[2][3],i,j,sum=0;
 	printf("Enter the elements: ");  
 	for(i=0; i<2; i++){     
-		for(j=0; j<3; j++){  
+		for(j=0; j<3; j++){   
 			scanf("%d", &a[i][j]);    
 		} 
 	}   

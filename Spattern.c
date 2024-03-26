fa323c4cb270341a900a72f67898a992c6a33d22
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-03-26 20:21:43 +0530

    Update Spattern.c

--- Spattern.c
+++ Spattern.c
@@ -2,7 +2,7 @@
 
 int main() {
     int i,j, height = 7;   
-
+ 
     for (i = 1; i <= height; i++) {  
         for (j = 1; j <= height; j++) {   
             if ((i == 1 || i == height || i == height / 2 + 1) ||  

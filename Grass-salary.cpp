0da7d482e6f001f7c22f550de195170aefc3550f
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-10-22 19:29:51 +0530

    Update Grass-salary.cpp

--- Grass-salary.cpp
+++ Grass-salary.cpp
@@ -6,7 +6,7 @@ main()
   printf("Enter Ramesh's Basic Salary : ");   
   scanf("%d",&bs); 
   da=bs*40/100;  
-  ra=bs*20/100;    
+  ra=bs*20/100;     
   gs=bs-da-ra; 
   printf("Gross Salary of Ramesh is %f",gs);   
   return 0;    
